a2cfe244341cac7942e480cfb7e9f9ee5f30e7c1
Royce Dy <rady182@me.com>
2016-03-12 14:44:49 +0800

    Added Initial Packages View Controller

---
+++ Pakete/PackagesViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PackagesViewController.swift
+//  Pakete
+//
+//  Created by Royce Albert Dy on 12/03/2016.
+//  Copyright © 2016 Pakete. All rights reserved.
+//
+
+import UIKit
+
+class PackagesViewController: UIViewController {
+    
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Packages"
+        
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.dataSource = self
+        self.view.addSubview(self.tableView)
+        NSLayoutConstraint.activateConstraints([
+            NSLayoutConstraint(item: self.tableView, attribute: .Top, relatedBy: .Equal, toItem: self.view, attribute: .Top, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: self.tableView, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: self.tableView, attribute: .Leading, relatedBy: .Equal, toItem: self.view, attribute: .Leading, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: self.tableView, attribute: .Trailing, relatedBy: .Equal, toItem: self.view, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
+        ])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+// MARK: UITableViewDataSource
+extension PackagesViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
