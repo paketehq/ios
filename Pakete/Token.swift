99cc63eabd85711111d545f1c013489ab39c6874
Royce Dy <rady182@me.com>
2016-04-15 15:54:57 +0800

    added token.swift

---
+++ Pakete/Token.swift
@@ -0,0 +1,55 @@
+//
+//  Token.swift
+//  Pakete
+//
+//  Created by Royce Albert Dy on 06/04/2016.
+//  Copyright © 2016 Pakete. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Security
+import CryptoSwift
+import Keys
+
+struct Token {
+    
+    private var iv: NSData!
+    private var key: NSData!
+    
+    init() {
+        let keys = PaketeKeys()
+        self.key = keys.paketeAPIKey().dataUsingEncoding(NSUTF8StringEncoding)
+        self.iv = randomIV()
+    }
+    
+    func tokenString() -> String {
+        return self.base64GeneratedEncryptedString()
+    }
+
+}
+
+private extension Token {
+    func randomIV() -> NSData {
+        let data = NSMutableData(length: self.key.length)!
+        let result = SecRandomCopyBytes(kSecRandomDefault, self.key.length, UnsafeMutablePointer<UInt8>(data.mutableBytes))
+        guard result == errSecSuccess else {
+            fatalError("SECURITY FAILURE: Could not generate secure random numbers: \(result).")
+        }
+        
+        return data
+    }
+    
+    func base64GeneratedEncryptedString() -> String {
+        guard let deviceId = UIDevice.currentDevice().identifierForVendor?.UUIDString else { return "" }
+        let timeInterval = NSDate().timeIntervalSince1970
+        let nonce = deviceId + ":\(timeInterval)"
+        let nonceData = nonce.dataUsingEncoding(NSUTF8StringEncoding)!
+        
+        let encrypted = try! nonceData.encrypt(AES(key: self.key.arrayOfBytes(), iv: self.iv.arrayOfBytes()))
+        let encryptedBase64 = encrypted.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+        
+        let base64IVString = self.iv.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
+        return "\(base64IVString):\(encryptedBase64)"
+    }
+}
