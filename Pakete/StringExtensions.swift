e0efb6aefe0b63a17d393e0661ff2c6a32d30581
Royce Dy <rady182@me.com>
2016-05-17 21:15:08 +0800

    added heightWithConstrainedWidth extension for String

--- Pakete/StringExtensions.swift
+++ Pakete/StringExtensions.swift
@@ -18,4 +18,10 @@ extension String {
         }
         return result
     }
+    
+    func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: CGFloat.max)
+        let boundingBox = self.boundingRectWithSize(constraintRect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        return boundingBox.height
+    }
 }
