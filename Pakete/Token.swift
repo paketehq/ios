8e8bbee16012e5914f9e5b8c1d2b6e5e9fcf4d50
Royce Dy <rady182@me.com>
2016-05-23 15:49:16 +0800

    use isEmpty

--- Pakete/Token.swift
+++ Pakete/Token.swift
@@ -19,7 +19,7 @@ struct Token {
     
     init() {
         let keys = PaketeKeys()
-        if keys.paketeAPIKey().characters.count > 0 {
+        if keys.paketeAPIKey().isEmpty == false {
             self.key = keys.paketeAPIKey().dataUsingEncoding(NSUTF8StringEncoding)
         } else {
             self.key = "1234567890123456".dataUsingEncoding(NSUTF8StringEncoding)
