249b94a8d8a04821ac064a231068a68fc5d72e1d
Royce Dy <rady182@me.com>
2016-05-30 21:22:05 +0800

    added constant for default keys

---
+++ Pakete/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pakete
+//
+//  Created by Royce Albert Dy on 30/05/2016.
+//  Copyright © 2016 Pakete. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Defaults {
+        static let SortByKey = "SortByKey"
+        static let GroupByDeliveredKey = "GroupByDeliveredKey"
+    }
+}
