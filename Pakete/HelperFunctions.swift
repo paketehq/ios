3fb785e0ba981387412423366540b66fe0b511bc
Royce Dy <rady182@me.com>
2016-06-06 12:33:18 +0800

    added function stringFromDate

--- Pakete/HelperFunctions.swift
+++ Pakete/HelperFunctions.swift
@@ -21,6 +21,12 @@ class PKDateHelper {
         self.dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         return self.dateFormatter.dateFromString(dateString)
     }
+    
+    func stringFromDate(date: NSDate, dateStyle: NSDateFormatterStyle, timeStyle: NSDateFormatterStyle) -> String {
+        self.dateFormatter.dateStyle = dateStyle
+        self.dateFormatter.timeStyle = timeStyle
+        return self.dateFormatter.stringFromDate(date)
+    }
 }
 
 func isNotEmptyString(string: String) -> Bool {
